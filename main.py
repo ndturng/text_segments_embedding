dcf25ee855c1975be5f7f1a6fccdc729fbcae31e
Nguyen Duong Trung <103947618+ndturng@users.noreply.github.com>
2024-03-26 07:56:48 +0000

    split text data and embed text chunk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfReader
-
+from sentence_transformers import SentenceTransformer
+from langchain.text_splitter import CharacterTextSplitter
 
 # Read the PDF file from: data/test.pdf
 pdf_file = "data/test.pdf"
@@ -11,4 +12,21 @@ text = ""
 for page in pdf_content.pages:
     text += page.extract_text()
 
-print(text)
+# Split the text into chunks
+splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=1000,
+    chunk_overlap=100,
+    length_function=len,
+)
+text_chunks = splitter.split_text(text)
+
+
+# Load the SBERT model
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)  # There are many other models available
+
+# Embed the text chunks
+embeddings = model.encode(text_chunks)
+print("Done")
